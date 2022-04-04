1327b93f2635bf40edbc404c8809a5c45455d4f9
Tierney Cyren <hello@bnb.im>
2022-04-04 17:40:21 -0400

    fix: make it work much better
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- generate/templates/generic.js
+++ generate/templates/generic.js
@@ -1,124 +1,106 @@
-async function generateGenericMarkdown (data, types, options = {}) {
+async function generateGenericMarkdown (data, options = {}) {
   const usableOptions = {
     heading: options.heading ?? `# Retrospective for \`${data.meta.owner}\``,
     preface: options.preface ?? `Reporting on ${data.meta.issues.count} Issues from ${data.meta.issues.authors.count} authors, ${data.meta.pullRequests.count} Pull Requests from ${data.meta.pullRequests.authors.count} authors, and ${data.meta.discussions.count} Discussions from ${data.meta.discussions.authors.count} authors.\n\n`
   }
 
-  const typeMarkdown = {}
-  for (const type of types) {
-    // define the heading depending on the type we get
-    let heading
-    let genericData
+  const repos = {}
 
-    if (type === 'issues') {
-      heading = 'Issues'
-      genericData = data.issues
-    } else if (type === 'prs') {
-      heading = 'Pull Requests'
-      genericData = data.pullRequests
-    }
-    if (type === 'discussions') {
-      heading = 'Discussions'
-      genericData = data.discussions
-    }
-
-    // static content
-    const headers = {
-      section: `## ${heading}`,
-      open: `#### Open ${heading}`,
-      merged: `#### Merged ${heading}`,
-      closed: `#### Closed ${heading}`
-    }
-
-    // dynamic content that will be populated
-    const bullets = {
-      open: [],
-      merged: [],
-      closed: []
-    }
-
-    const sections = {}
-
-    // processing each issue and populating the data that we use to construct markdown
-    genericData.forEach(instance => {
+  for (const type in data) {
+    if (type === 'issues' || type === 'pullRequests' || type === 'discussions') {
+      data[type].forEach(instance => {
       // generate the bullet, since we're always going to use it
-      const bullet = `- ${instance.meta.title} ([${instance.meta.number}](${instance.meta.url}))`
+        const bullet = `- ${instance.meta.title} ([${instance.meta.number}](${instance.meta.url}))`
 
-      // if this is the first time we're encountering the repo, create a section for into
-      if (sections[instance.repo.name] === undefined) {
-        sections[instance.repo.name] = {
-          open: [],
-          merged: [],
-          closed: []
+        // if this is the first time we're encountering the repo, create a section for into
+        if (repos[instance.repo.name] === undefined) {
+          repos[instance.repo.name] = {
+            open: [],
+            merged: [],
+            closed: []
+          }
         }
-      }
 
-      // populates the appropriate array with the appropriate markdown:
-      if ((instance.meta.type === 'Discussion') || (instance.status.state === 'OPEN' && !instance.status.locked)) {
-        bullets.open.push(bullet)
-        sections[instance.repo.name].open.push({ bullet })
-      } else if (instance.status.state === 'MERGED') {
-        bullets.merged.push(bullet)
-        sections[instance.repo.name].merged.push({ bullet })
-      } else if (instance.status.state === 'CLOSED' && !instance.status.locked) {
-        bullets.closed.push(bullet)
-        sections[instance.repo.name].closed.push({ bullet })
-      }
-    })
+        // populates the appropriate array with the appropriate markdown:
+        if ((instance.meta.type === 'Discussion') || (instance.status.state === 'OPEN' && !instance.status.locked)) {
+          repos[instance.repo.name].open.push({ bullet, type: instance.meta.type })
+        } else if (instance.status.state === 'MERGED') {
+          repos[instance.repo.name].merged.push({ bullet, type: instance.meta.type })
+        } else if (instance.status.state === 'CLOSED' && !instance.status.locked) {
+          repos[instance.repo.name].closed.push({ bullet, type: instance.meta.type })
+        }
+      })
+    }
+  }
 
-    // concat all the strings we've built into a markdown piece
-    let markdown = ''
+  // concat all the strings we've built into a markdown piece
+  let markdown = ''
 
-    // cache the headings we've already created so we don't dupe them
-    const garbageCache = []
+  // cache the headings we've already created so we don't dupe them
+  const garbageCache = []
 
-    // I am sorry for how gross this is
-    for (const repo in sections) { // go over every repo in the sections object
-      const heading = `### ${repo}`
-      // write the heading and push the heading to the
-      // headings object if it's not there so we only write it once
-      //
-      // this is a weird hack but it seems to work so... meh
-      if (!garbageCache.includes(heading)) {
-        garbageCache.push(heading)
-        markdown = markdown.concat(`\n${heading}\n`)
-      }
-      // loop over the states (open, merged closed) in each repo
-      for (const state in sections[repo]) {
-        if (state === 'open') {
+  // I am sorry for how gross this is
+  for (const repo in repos) { // go over every repo in the sections object
+    const heading = `### ${repo}`
+    // write the heading and push the heading to the
+    // headings object if it's not there so we only write it once
+    //
+    // this is a weird hack but it seems to work so... meh
+    if (!garbageCache.includes(heading)) {
+      garbageCache.push(heading)
+      markdown = markdown.concat(`\n${heading}\n`)
+    }
+    // loop over the states (open, merged closed) in each repo
+    for (const state in repos[repo]) {
+      if (state === 'open') {
+        for (const instance in repos[repo][state]) {
           // same ugly hack as above, just using headings as a store that we can check against before writing the heading
           // write each bullet out
-          for (const instance in sections[repo][state]) {
-            if (!garbageCache.includes(`${heading} ${state}`)) {
-              garbageCache.push(`${heading} ${state}`)
-              markdown = markdown.concat(`\n${headers.open}\n`)
+          if (!garbageCache.includes(`${heading} ${state}`)) {
+            garbageCache.push(`${heading} ${state}`)
+            if (repos[repo][state][instance].type === 'Issue') {
+              const localHeading = await generateSectionHeader(4, 'Issues', state)
+              markdown = markdown.concat(localHeading)
             }
-            markdown = markdown.concat(`${sections[repo][state][instance].bullet}\n`)
+
+            if (repos[repo][state][instance].type === 'PullRequest') {
+              const localHeading = await generateSectionHeader(4, 'Pull Requests', state)
+              markdown = markdown.concat(localHeading)
+            }
           }
-        } else if (state === 'merged') {
+          markdown = markdown.concat(`${repos[repo][state][instance].bullet}\n`)
+        }
+      } else if (state === 'merged') {
+        for (const instance in repos[repo][state]) {
           // same ugly hack as above, just using headings as a store that we can check against before writing the heading
           // write each bullet out
-          for (const instance in sections[repo][state]) {
-            if (!garbageCache.includes(`${heading} ${state}`)) {
-              garbageCache.push(`${heading} ${state}`)
-              markdown = markdown.concat(`\n${headers.merged}\n`)
-            }
-            markdown = markdown.concat(`${sections[repo][state][instance].bullet}\n`)
+          if (!garbageCache.includes(`${heading} ${state}`)) {
+            garbageCache.push(`${heading} ${state}`)
+            const localHeading = await generateSectionHeader(4, 'Issues', state)
+            markdown = markdown.concat(localHeading)
           }
-        } else if (state === 'closed') {
+          markdown = markdown.concat(`${repos[repo][state][instance].bullet}\n`)
+        }
+      } else if (state === 'closed') {
+        for (const instance in repos[repo][state]) {
           // same ugly hack as above, just using headings as a store that we can check against before writing the heading
           // write each bullet out
-          for (const instance in sections[repo][state]) {
-            if (!garbageCache.includes(`${heading} ${state}`)) {
-              garbageCache.push(`${heading} ${state}`)
-              markdown = markdown.concat(`\n${headers.closed}\n`)
+          if (!garbageCache.includes(`${heading} ${state}`)) {
+            garbageCache.push(`${heading} ${state}`)
+            if (repos[repo][state][instance].type === 'Issue') {
+              const localHeading = await generateSectionHeader(4, 'Issues', state)
+              markdown = markdown.concat(localHeading)
             }
-            markdown = markdown.concat(`${sections[repo][state][instance].bullet}\n`)
+
+            if (repos[repo][state][instance].type === 'PullRequest') {
+              const localHeading = await generateSectionHeader(4, 'Pull Requests', state)
+              markdown = markdown.concat(localHeading)
+            }
           }
+          markdown = markdown.concat(`${repos[repo][state][instance].bullet}\n`)
         }
       }
     }
-    typeMarkdown[type] = markdown
   }
 
   // generate the final string we're going to send back
@@ -127,16 +109,26 @@ async function generateGenericMarkdown (data, types, options = {}) {
   output = output.concat(`${usableOptions.preface}`)
   output = output.trim()
   output = output.concat('\n')
-  // loop over the objects that we've generated
-  for (const type in typeMarkdown) {
-    output = output.concat(typeMarkdown[type])
-  }
-
-  console.log(JSON.stringify(typeMarkdown, null, 2))
-  // trim beginning/ending whitespace
+  output = output.concat(markdown)
   output = output.trim()
 
   return output
+}
+
+async function generateSectionHeader (level, type, state) {
+  let header = '\n'
+  for (let i = 0; i < level; i++) {
+    header = header.concat('#')
+  }
+  if (state !== undefined) {
+    const capitalizedState = state.charAt(0).toUpperCase() + state.slice(1)
+    header = header.concat(' ', capitalizedState, ' ', type)
+  } else {
+    header = header.concat(' ', type)
+  }
+  header = header.concat('\n\n')
+
+  return header
 }
 
 module.exports = generateGenericMarkdown
