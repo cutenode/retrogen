2aa725203ed7bc63c849a452dc0536d4c53416c6
Tierney Cyren <hello@bnb.im>
2022-09-18 21:19:25 -0400

    chore: stash for wipe
    Signed-off-by: Tierney Cyren <hello@bnb.im>

---
+++ generate/examples/defaultButJson.js
@@ -0,0 +1,14 @@
+const generate = require('@retrogen/generate')
+
+async function markdown () {
+  // the date range we want to get information for
+  const dates = {
+    start: '2022-03-27',
+    end: '2022-04-02'
+  }
+
+  // this will generate markdown for the Node.js GitHub organization that includes data for Issues, PRs, and Discussions from 2022-03-27 to 2022-04-02
+  await generate('nodejs', dates, { log: true, json: true })
+}
+
+markdown()
--- generate/index.js
+++ generate/index.js
@@ -7,16 +7,51 @@ async function generate (owner, dates, options) {
   const usableOptions = {
     headerDates: options?.headerDates ?? true,
     start: dates.start,
-    end: dates.end
+    end: dates.end,
+    log: options?.log ?? false,
+    json: options?.json ?? false,
+    includeData: options?.includeData ?? false,
   }
+
+  // handle the situation where someone does includeData but doesn't output as JSON
+  if(usableOptions.includeData === true && usableOptions.json === false) throw new Error('You must output as JSON if you want to use the includeData option.')
+  
+  // collect and build our data and output
   const data = await core(owner, usableOptions)
   const markdown = await templates('basic', data, usableOptions)
-
-  if (options?.log) {
-    console.log(markdown)
+  const returnable = {
+    data: data,
+    markdown: markdown
   }
 
-  return markdown
+  // handle various options
+  if (usableOptions.json === true && usableOptions.includeData === true) { // both JSON and includeData
+    const returnedJSON = JSON.stringify(returnable, null, 2)
+
+    if (usableOptions.log) {
+      console.log(returnedJSON)
+    }
+
+    return returnedJSON
+  } else if(usableOptions.json === true && usableOptions.includeData === false) { // only JSON
+    const slimmedReturnable = {
+      markdown: returnable.markdown
+    }
+
+    const returnedJSON = JSON.stringify(slimmedReturnable, null, 2)
+
+    if (usableOptions.log) {
+      console.log(slimmedReturnable)
+    }
+  
+    return slimmedReturnable
+  } else { // default output
+    if (usableOptions.log) {
+      console.log(returnable.markdown)
+    }
+
+    return returnable.markdown
+  }
 }
 
 module.exports = generate
